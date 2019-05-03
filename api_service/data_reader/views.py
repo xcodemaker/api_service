0fa54e9034d5461714175fea00732dcd556ec41c
xcodemaker <dhammika.dsk@gmail.com>
2019-05-03 22:09:55 +0530

    url changed

--- api_service/data_reader/views.py
+++ api_service/data_reader/views.py
@@ -12,7 +12,7 @@ import requests
 from api_service.settings import BASE_DIR
 
 
-IMPORT_URL = 'http://data-service-deployment'
+IMPORT_URL = 'http://data-service-deployment:8000'
 
 @api_view(["GET"])
 def getState(request):
